8feded4e890e2ba73b157f69829f247cde2aadfe
szleaves <steven-zh@foxmail.com>
2024-07-01 19:29:37 +0800

    update: format

--- c/C2/2-12-3.c
+++ c/C2/2-12-3.c
@@ -1,14 +1,18 @@
-//2.12.3--Test P37
-#include<stdio.h>
-int main(void)
-{
-int years;
-int olds;
-printf("Please enter your olds: \n");
-scanf("%d",&olds);
-years = olds * 365;
-printf("So I lived in the earth %d days. \n",years);
-printf("Fu@k you.\n");
+// 2.12.3--Test P37
+#include <stdio.h>
+int main(void) {
+    int years;
+    int olds;
+    printf("Please enter your olds: \n");
+    scanf("%d", &olds);
+    years = olds * 365;
+    printf("So I lived in the earth %d days. \n", years);
+    printf("Fu@k you.\n");
 
-return 0;
+    return 0;
+}
+
+int func(int abc, int abc_ds) {
+    printf("%d", 1);
+    return 0;
 }
